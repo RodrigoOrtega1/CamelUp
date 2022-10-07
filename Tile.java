9bc8a51ba1f6565a607d3797bd63c7de07d03e78
Rodrigo Ortega <rodrigoortega@ciencias.unam.mx>
2022-10-07 10:11:37 -0500

    Added getters and setters to Tile

--- Tile.java
+++ Tile.java
@@ -1,4 +1,20 @@
 public class Tile {
     boolean hasModifier = false;
+    boolean modifier;
     SimpleLinkedList<Camel> camelStack = new SimpleLinkedList<>();
+    
+    public boolean getHasModifier() {
+        return hasModifier;
+    }
+    public void setHasModifier(boolean hasModifier) {
+        this.hasModifier = hasModifier;
+    }
+    public boolean getModifier() {
+        return modifier;
+    }
+    public void setModifier(boolean modifier) {
+        this.modifier = modifier;
+    }
+    
+
 }
